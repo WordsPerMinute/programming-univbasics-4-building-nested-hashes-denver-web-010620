e9222b5dd9a022aa6e42f5f4a48c8661b195cafe
Corey Hodge <contact@coreyhodge.net>
2019-12-29 03:54:49 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,9 +1,9 @@
 def base_hash
-	monopoly = {railroads: {}}
+		monopoly = {railroads: {}}
 end
 
 def monopoly_with_second_tier
-		monopoly = {railroads: {}}
+		monopoly = {railroads: {}, pieces: "shoe"}
 end
 
 def monopoly_with_third_tier
